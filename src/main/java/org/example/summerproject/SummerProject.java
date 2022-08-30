25c8a12c69f754890bc367fd65a4236bb0dc65bf
XRafe <nikita-gorin98@yandex.ru>
2022-08-30 13:48:38 +0400

    init

---
+++ src/main/java/org/example/summerproject/SummerProject.java
@@ -0,0 +1,12 @@
+package org.example.summerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerProject {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummerProject.class);
+    }
+}
